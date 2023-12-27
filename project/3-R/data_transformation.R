06465d2ebad158d50cf5c1440eae4aba0415e583
Matas Vinkus <Matas.Vinkus@socmin.lt>
2023-12-27 20:44:13 +0200

    update

--- project/3-R/data_transformation.R
+++ project/3-R/data_transformation.R
@@ -1,3 +1,21 @@
-library(readr)
-read_csv("../1-data/put_your_data_files_here.csv")
-### save combined file into 1-data directory
+#install.packages("tidyverse")
+library(tidyverse)
+library(visdat)
+library(dplyr)
+getwd()
+setwd("C://Users//ievak//OneDrive//Documents//Projektas//KTU-DVDA-PROJECT-main//project")
+
+file_path <- ("1-data/1-sample_data.csv")
+data <- read_csv(file_path)
+data_additional <- read_csv("1-data/2-additional_data.csv")
+data_feat <- read_csv("1-data/3-additional_features.csv")
+
+
+#Joins
+combined_df <- dplyr::bind_rows(data_additional, data)
+df <- inner_join(combined_df, data_feat, by = "id")
+
+
+
+#Writing procedure
+write_csv(df, "1-data/train_data.csv")
